b33eaf3664a9e7fc4b26cc6bbfc2e89d8931e100
Bernd Hufmann <Bernd.Hufmann@ericsson.com>
2020-06-04 17:04:16 -0400

    Add support for docker image using ubuntu-18.04 and test jenkins file
    Change-Id: Ieb2b372d6e7229c54eaf748a95f479be167a7a32
    Signed-off-by: Bernd Hufmann <Bernd.Hufmann@ericsson.com>

---
+++ docker/ubuntu-18.04/scripts/xstartup_icewm.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env /bin/sh
+###############################################################################
+# Copyright (c) 2019 Ericsson.
+#
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License 2.0
+# which accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-2.0/
+# 
+# SPDX-License-Identifier: EPL-2.0
+###############################################################################
+
+[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+[ -r ${HOME}/.Xresources ] && xrdb ${HOME}/.Xresources
+
+Xvnc ${DISPLAY} -geometry 1440x900 -depth 16 -dpi 100 -PasswordFile ${HOME}/.vnc/passwd &
+sleep 2
+xsetroot -solid grey
+vncconfig -iconic &
+xhost +
+icewm --replace --sm-disable --display=${DISPLAY} &
