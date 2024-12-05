e322064dd1e6b3af6309262bc846c3dfe0e16451
Marc Dumais <marc.dumais@ericsson.com>
2024-12-05 13:12:34 -0500

    adapt the 'du' command to go through ssh
    Signed-off-by: Marc Dumais <marc.dumais@ericsson.com>

--- scripts/generate_download_page.sh
+++ scripts/generate_download_page.sh
@@ -115,7 +115,8 @@ files=($(${SSH} "find ${src_dir} -maxdepth 1 -type f -not -name 'config.php' -no
 # Loop through files, generate a table entry for each one
 for file in "${files[@]}"; do
     filename=$(basename $file)
-    size=$(du -h "$file" | awk '{print $1}')
+    size=($(${SSH} "du -h '${src_dir}/$file' | awk '{print $1}'"))
+
     # Something to try... It might work to rely on the browser's context, 
     # and omit the server and path completely - after all the browser will 
     # already need to know these things to load the generated page. It this 
